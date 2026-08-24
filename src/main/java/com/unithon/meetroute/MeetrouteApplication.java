e3900b95e44c4b34a0af1dc4ed2dc929692462bd
HarryBae1011 <06harry@naver.com>
2026-08-24 19:00:11 +0900

    settings: initial setting

---
+++ src/main/java/com/unithon/meetroute/MeetrouteApplication.java
@@ -0,0 +1,13 @@
+package com.unithon.meetroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetrouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unithon/meetroute/MeetrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unithon.meetroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
